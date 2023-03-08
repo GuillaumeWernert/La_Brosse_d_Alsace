53ac8f64ca44b097474417693073994b378be215
guillaume wernert <guil.wernert@gmail.com>
2023-03-08 04:34:49 +0100

    home desktop

--- frontend/src/components/SliderPresta.jsx
+++ frontend/src/components/SliderPresta.jsx
@@ -8,6 +8,7 @@ import Fumigene from "@assets/fumigene.jpg";
 import Poele from "@assets/poele.jpg";
 import Videoscopie from "@assets/videoscopie.jpg";
 import Vmc from "@assets/vmc.jpg";
+import Chaudiere from "@assets/chaudiere.jpg";
 
 export default function SliderPresta() {
   return (
@@ -73,6 +74,13 @@ export default function SliderPresta() {
             className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
             aria-label="Slide 6"
           />
+          <button
+            type="button"
+            data-te-target="#carouselExampleCrossfade"
+            data-te-slide-to="6"
+            className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+            aria-label="Slide 7"
+          />
         </div>
         <div className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
           <div
@@ -129,6 +137,13 @@ export default function SliderPresta() {
             data-te-carousel-item
           >
             <img src={Cheminee} className="block w-full" alt="Exotic Fruits" />
+          </div>
+          <div
+            className="relative float-left -mr-[100%] hidden w-full !transform-none opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none"
+            data-te-carousel-fade
+            data-te-carousel-item
+          >
+            <img src={Chaudiere} className="block w-full" alt="Exotic Fruits" />
           </div>
         </div>
         <button
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import "tw-elements";
 
-import SliderPresta from "@components/SliderPresta"
+import SliderPresta from "@components/SliderPresta";
 import Background from "@assets/bg_feu.jpeg";
 import Zone from "@assets/zone.jpg";
+import Cheminee from "@assets/cheminee.jpg";
+import Conduit from "@assets/conduit.jpg";
+import Debistrage from "@assets/debistrage.jpeg";
+import Fumigene from "@assets/fumigene.jpg";
+import Poele from "@assets/poele.jpg";
+import Videoscopie from "@assets/videoscopie.jpg";
+import Vmc from "@assets/vmc.jpg";
+import Chaudiere from "@assets/chaudiere.jpg";
 
 export default function Home() {
   return (
     <div className="home">
-      <img src={Background} alt="feu de cheminée" className="w-full" />
+      <img src={Background} alt="feu de cheminée" className="w-full h-96" />
       <div className="presentation">
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur,
@@ -19,23 +27,65 @@ export default function Home() {
           Delectus deleniti sunt voluptatem laborum, vitae voluptatibus!
         </p>
       </div>
-      <div className="prestations">
+      <div className="prestations w-full">
         <h1 className="text-center">NOS PRESTATIONS</h1>
-        <p>Conduits de fumées</p>
-        <p>Chaudières</p>
-        <p>Cheminées à foyer</p>
-        <p>Poêle en faience</p>
-        <p>Contrôle d'étanchéité</p>
-        <p>Vidéoscopie des conduits</p>
-        <p>Ventilation mécanique contrôlée (VMC)</p>
-        <p>Débistrage des conduits de fumée</p>
-      </div>
-      <div>
-        <SliderPresta />
+        <div className="hidden lg:inline-block w-full h-full">
+          <div className="flex mt-5 mb-10 h-full">
+            <div className="w-4/12 h-full">
+              <div className="conduits w-full">
+                <p className="text-center">Conduits de fumées</p>
+                <img src={Conduit} alt="" className="w-full" />
+              </div>
+              <div className="chaudiere w-full h-full">
+                <p className="text-center">Chaudières</p>
+                <img src={Chaudiere} alt="" className="h-full" />
+              </div>
+            </div>
+            <div className="cheminee w-4/6">
+              <p className="text-center">Cheminées à foyer</p>
+              <img src={Cheminee} alt="" className="h-full" />
+            </div>
+          </div>
+          <div className="flex">
+            <div className="poele w-6/12">
+              <p className="text-center">Poêle en faience</p>
+              <img src={Poele} alt="" />
+            </div>
+            <div className="fumigene w-6/12">
+              <p className="text-center">Contrôle d'étanchéité</p>
+              <img src={Fumigene} alt="" />
+            </div>
+          </div>
+          <div className="flex">
+            <div className="vmc w-4/6">
+              <p className="text-center">
+                Ventilation mécanique contrôlée (VMC)
+              </p>
+              <img src={Vmc} alt="" />
+            </div>
+            <div>
+              <div className="videoscopie">
+                <p className="text-center">Vidéoscopie des conduits</p>
+                <img src={Videoscopie} alt="" className="w-full" />
+              </div>
+              <div className="debistrage">
+                <p className="text-center">Débistrage des conduits de fumée</p>
+                <img src={Debistrage} alt="" className="w-full" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="lg:hidden">
+          <SliderPresta />
+        </div>
       </div>
       <div className="zone">
         <h1 className="text-center">NOTRE ZONE D'INTERVENTION</h1>
-        <img src={Zone} alt="carte zone intervention" className="w-full" />
+        <img
+          src={Zone}
+          alt="carte zone intervention"
+          className="w-full h-screen"
+        />
       </div>
       <div className="references">
         <h1 className="text-center">NOS REFERENCES</h1>
